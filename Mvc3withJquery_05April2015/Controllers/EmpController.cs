198d6d50e5bff761a56a6d02ef1d4d37a543bb23
Deepak Vashishitha <dpk_vash@yahoo.co.in>
2016-12-24 20:25:27 +0530

    Initial checkin

---
+++ Mvc3withJquery_05April2015/Controllers/EmpController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc3withJquery_05April2015.EF;
+
+namespace Mvc3withJquery_05April2015.Controllers
+{
+    public class EmpController : Controller
+    {
+        //
+        // GET: /Emp/
+        AbhiEntities db;
+        public EmpController()
+        {
+            db = new AbhiEntities();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [OutputCache(Duration = 0)]
+        public JsonResult GetEmployee()
+        {
+            return Json(db.Employees.ToList(), JsonRequestBehavior.AllowGet);
+        }
+        
+        public JsonResult AddEmployee(Employee Emp)
+        {
+            db.Employees.Add(Emp);
+            db.SaveChanges();
+            return Json("Record has Inserted.", JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult DeleteEmployee(int EmpId)
+        {
+            Employee Emp =db.Employees.First(em=>em.EmpId==EmpId);
+            db.Employees.Remove(Emp);
+            db.SaveChanges();
+            return Json("Record has Deleted.", JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult UpdateEmployee(Employee Emp)
+        {
+           Employee emp =db.Employees.First(em=>em.EmpId==Emp.EmpId);
+           emp.EmpName = Emp.EmpName;
+           emp.EmpSalary = Emp.EmpSalary;
+            db.SaveChanges();
+            return Json("Record has Updated.", JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Mvc3withJquery_05April2015/Scripts/EmpIndex.js
@@ -0,0 +1,109 @@
+﻿
+var GDATA = "";
+function getEmployee() {
+    $.get('Emp/GetEmployee', null, function (data) { chkGetEmployee(data); });
+}
+function chkGetEmployee(abc) {
+    GDATA = abc;
+    grid(GDATA, 0);
+}
+function grid(abc, eid) {
+    var tab = "<table>";
+    tab += "<tr>";
+    tab += "<td style='width:100px;background-color:silver;'>Emp Id</td>";
+    tab += "<td style='width:100px;background-color:silver;'>Emp Name</td>";
+    tab += "<td style='width:100px;background-color:silver;'>Emp Salary</td>";
+    tab += "<td style='width:100px;background-color:silver;'>Updation</td>";
+    tab += "<td style='width:100px;background-color:silver;'>Operation</td>";
+    tab += "</tr>";
+    for (var it in abc) {
+        tab += "<tr>";
+
+        tab += "<td style='width:100px;background-color:silver;'>" + abc[it].EmpId + "</td>";
+
+        if (abc[it].EmpId != eid) {
+            tab += "<td style='width:100px;background-color:silver;'>" + abc[it].EmpName + "</td>";
+            tab += "<td style='width:100px;background-color:silver;'>" + abc[it].EmpSalary + "</td>";
+            tab += "<td style='width:100px;background-color:silver;'>"
+            + "<input onclick='EditEmployee(" + abc[it].EmpId + ")' id='btnEdit' type=button value='Edit' /></td>";
+        }
+        else {
+            tab += "<td style='width:100px;background-color:silver;'>"
+                + "<input id='txtEditEmpName' type='text' value='" + abc[it].EmpName + "' /></td>";
+            tab += "<td style='width:100px;background-color:silver;'>"
+                + "<input id='txtEditEmpSalary' type='text' value='" + abc[it].EmpSalary + "' /></td>";
+            tab += "<td style='width:100px;background-color:silver;'>"
+            + "<input onclick='UpdateEmployee(" + abc[it].EmpId + ")' id='btnUpdate' type=button value='Update' />"
+            + "<input onclick='CancelEmployee()' id='btnCancel' type=button value='Cancel' /></td>";
+
+        }
+
+        tab += "<td style='width:100px;background-color:silver;'>"
+            + "<input onclick='DeleteEmployee(" + abc[it].EmpId + ")' id='btnDelete' type=button value='Delete' /></td>";
+
+
+        tab += "</tr>";
+    }
+    tab += "<tr>";
+    tab += "<td style='width:100px;background-color:silver;'>"
+        + "<input id='txtEmpId' type='text' /></td>";
+    tab += "<td style='width:100px;background-color:silver;'>"
+        + "<input id='txtEmpName' type='text' /></td>";
+    tab += "<td style='width:100px;background-color:silver;'>"
+        + "<input id='txtEmpSalary' type='text' /></td>";
+    tab += "<td style='width:100px;background-color:silver;'>"
+      + "</td>";
+    tab += "<td style='width:100px;background-color:silver;'>"
+        + "<input onclick='AddEmployee()' id='btnSave' type=button value='Save' /></td>";
+
+    tab += "</tr>";
+    tab += "</table>";
+    $('#d1').html(tab);
+}
+
+function EditEmployee(eid) {
+    grid(GDATA, eid);
+}
+function CancelEmployee() {
+    grid(GDATA, 0);
+}
+
+function UpdateEmployee(eid) {
+    var empName = $('#txtEditEmpName').val();
+    var empSalary = $('#txtEditEmpSalary').val();
+    $.post('Emp/UpdateEmployee',
+        { EmpId: eid, EmpName: empName, EmpSalary: empSalary },
+        function (data) { chkUpdateEmployee(data); });
+}
+function chkUpdateEmployee(abc) {
+    alert(abc);
+    getEmployee();
+}
+
+
+//Coding For Add Record.
+function AddEmployee() {
+    var empId = $('#txtEmpId').val();
+    var empName = $('#txtEmpName').val();
+    var empSalary = $('#txtEmpSalary').val();
+    $.post('Emp/AddEmployee',
+        { EmpId: empId, EmpName: empName, EmpSalary: empSalary },
+        function (data) { chkAddEmployee(data); });
+}
+function chkAddEmployee(abc) {
+    alert(abc);
+    getEmployee();
+}
+
+function DeleteEmployee(eid) {
+    if (!confirm('Do you want to Delete.')) {
+        return;
+    }
+    $.post('Emp/DeleteEmployee',
+        { EmpId: eid },
+        function (data) { chkDeleteEmployee(data); });
+}
+function chkDeleteEmployee(abc) {
+    alert(abc);
+    getEmployee();
+}
